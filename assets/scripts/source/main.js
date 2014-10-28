25b921ffcc7258f630de4387a1c9b5237036c215
Guilherme Barchik <guilherme.barchik@casa.ag>
2014-10-28 09:51:22 -0200

    Add a script to load LiveReload if is localhost

--- assets/scripts/source/main.js
+++ assets/scripts/source/main.js
@@ -7,6 +7,16 @@
 	
 	/* Triggers
 	------------------------ */
+	// Document Ready
+	$(document).ready( function() {
+		// LiveReload
+		if( location.hostname.indexOf( 'localhost' ) !== -1 ) {
+			var liveReload = $( '<script />' );
+			liveReload
+				.attr( 'src', '//localhost:35729/livereload.js' )
+				.appendTo( 'body' );
+		}
+	});
 	
 	
 	
